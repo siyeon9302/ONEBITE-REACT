91029a56afe1862a9b06e463d502551893d0ea47
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-02-04 22:56:59 +0900

    feat: Promise 완료

---
+++ section02/chapter13.js
@@ -0,0 +1,33 @@
+function add10(num) {
+  // 안에서 프로미스 객체가 생성되는 중
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업 실행하는 함수
+    // executor
+
+    setTimeout(() => {
+      if (typeof num === "number") {
+        resolve(num + 10);
+      } else {
+        reject("num이 숫자가 아닙니다");
+      }
+    }, 2000);
+  });
+
+  return promise; // 프로미스 객체 반환
+}
+
+add10(0) // 반환된 프로미스 객체가 저장됨
+  .then((result) => {
+    console.log(result);
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+    return add10(undefined);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
