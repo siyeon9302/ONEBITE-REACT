2db7c8c79785fff79c069c8dcb77e460d1e5034d
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-02-18 17:28:16 +0900

    feat: section03 끝

---
+++ section03/src/index.js
@@ -0,0 +1,8 @@
+import mul, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 3));
+// console.log(sub(5, 3));
